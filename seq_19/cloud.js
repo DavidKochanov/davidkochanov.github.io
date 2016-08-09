488b46a1e208fe9269620f55766a0c702583ea6e
deyvid.kochanov <david.kochanov@gmail.com>
2016-08-09 16:05:16 +0300

    First commit: add point clouds add basic potree structure add basic electron structure

---
+++ seq_19/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 1303338,
+    "boundingBox": {
+        "lx": -38.04999923706055,
+        "ly": -8.25,
+        "lz": 3.1500000953674318,
+        "ux": 209.15000677108766,
+        "uy": 238.9500060081482,
+        "uz": 250.35000610351563
+    },
+    "tightBoundingBox": {
+        "lx": -38.04999923706055,
+        "ly": -8.25,
+        "lz": 3.1500000953674318,
+        "ux": 12.75,
+        "uy": 2.6500000953674318,
+        "uz": 250.35000610351563
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 2.140814781188965,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
